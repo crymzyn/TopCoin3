e29c1e280b3adff5f48665f47549b6464d4b9ca2
crymzyn <crymzyn@gmail.com>
2020-09-17 12:12:11 -0500

    Adding PruneOrphanBlocks() to help with sync
    See bitcoin/bitcoin@bbde1e9

--- src/main.h
+++ src/main.h
@@ -29,6 +29,8 @@ static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
+/** The maximum number of orphan blocks kept in memory */
+static const unsigned int MAX_ORPHAN_BLOCKS = 750;
 static const unsigned int MAX_INV_SZ = 50000;
 static const int64 MIN_TX_FEE = 1.0 * CENT;
 static const int64 MIN_RELAY_TX_FEE = 1.0 * CENT;
